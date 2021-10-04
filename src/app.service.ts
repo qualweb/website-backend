719d87c7a3e6490a3b9c6f67deacd887b88bc726
João Vicente <joao.vicente@campus.ul.pt>
2021-10-04 17:09:20 +0100

    Updated dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { QualWeb, QualwebOptions, EvaluationReport } from '@qualweb/core';
 @Injectable()
 export class AppService {
   async evaluate(options: QualwebOptions): Promise<EvaluationReport> {
-    const qualweb = new QualWeb({ adBlock: false, stealth: false });
+    const qualweb = new QualWeb({ adBlock: false, stealth: true });
 
     await qualweb.start(
       { timeout: 1000 * 60 * 2 },
@@ -14,7 +14,7 @@ export class AppService {
     let reports = null;
     let error = null;
     try {
-      reports = await qualweb.evaluate(options); //this.timeout(qualweb, options);
+      reports = await qualweb.evaluate(options);
     } catch (err) {
       error = err;
     } finally {
@@ -26,21 +26,5 @@ export class AppService {
     }
 
     return reports[decodeURIComponent(options.url)];
-  }
-
-  private timeout(
-    qualweb: QualWeb,
-    options: QualwebOptions,
-  ): Promise<EvaluationReport[]> {
-    return new Promise<EvaluationReport[]>((resolve: any, reject: any) => {
-      const timeout = setTimeout(() => {
-        reject('Time exceeded for evaluation');
-      }, 1000 * 60);
-
-      qualweb.evaluate(options).then((reports) => {
-        clearTimeout(timeout);
-        resolve(reports);
-      });
-    });
   }
 }
